808ff99534a3fd95eb5a5ce489f313c10f4537fc
Nimil Peethambaran <nimil.peethambaran@iq-ity.com>
2017-02-22 14:36:51 -0500

    test case for build menu

--- js/test/scratchPadSpec.js
+++ js/test/scratchPadSpec.js
@@ -1,5 +1,14 @@
-describe('test',function(){
-    it('testing ', function(){
-        
+describe('Test ScratchPad',function(){
+    it('tests buildMenu ', function(){
+       
+        $("<div id='test'></div>").appendTo('body');
+        var instance = {};
+        instance.id=1;
+        instance.wrapper =  $('#test');
+        instance.menu = ScratchPad.defaultMenu;
+        ScratchPad.buildMenu(instance);
+        var $menu = $(instance.wrapper).find(".sp-menu");
+        expect($menu.html()).toContain('<div class="sp-drawing">Drawing</div><div class="sp-eraser">Eraser</div>');
     });
+    
 });

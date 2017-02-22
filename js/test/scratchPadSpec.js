0b7a06785e11f103a1701dd8cc408edb362f5c01
Nimil Peethambaran <nimil.peethambaran@iq-ity.com>
2017-02-22 14:53:52 -0500

    more test cases

--- js/test/scratchPadSpec.js
+++ js/test/scratchPadSpec.js
@@ -1,7 +1,10 @@
 describe('Test ScratchPad',function(){
+    beforeEach(function(){
+        $("<div id='test'></div>").appendTo('body');
+    });
     it('tests buildMenu ', function(){
        
-        $("<div id='test'></div>").appendTo('body');
+        
         var instance = {};
         instance.id=1;
         instance.wrapper =  $('#test');
@@ -11,4 +14,34 @@ describe('Test ScratchPad',function(){
         expect($menu.html()).toContain('<div class="sp-drawing">Drawing</div><div class="sp-eraser">Eraser</div>');
     });
     
+    it('tests init', function(){
+        var instance = {id:1, canvas:{}};
+        spyOn(ScratchPad,'buildInstance').andCallFake(function(){
+            return instance;
+        })
+        spyOn(ScratchPad,'buildMenu').andCallFake(function(){});
+        spyOn(ScratchPad,'buildPad').andCallFake(function(){ return {};});
+        ScratchPad.init('#test',{});
+        expect(ScratchPad.buildInstance).toHaveBeenCalledWith('#test',{});
+        expect(ScratchPad.buildMenu).toHaveBeenCalledWith(instance);
+        expect(ScratchPad.buildPad).toHaveBeenCalledWith(instance);
+        expect(ScratchPad.instances[1]).toBe(instance);
+    });
+    
+    it('tests buildInstance', function(){
+       var instance = ScratchPad.buildInstance('#test',{});
+        expect(instance.id).not.toBeNull();
+        expect(instance.menu).toBe(ScratchPad.defaultMenu);
+        expect(instance.dimension).toBe(ScratchPad.defaultDimension);
+    });
+    
+    it('tests buildInstance with a config', function(){
+        var dimension = {width:10, height:20};
+        var menu = ["menu1",  "menu2"];
+        var config = {menu:menu, dimension:dimension};
+        var instance = ScratchPad.buildInstance('test', config);
+        expect(instance.id).not.toBeNull();
+        expect(instance.menu).toBe(menu);
+        expect(instance.dimension).toBe(dimension);
+    });
 });

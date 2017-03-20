9bd469f5cf278690e4f4bc1f88806db543910cdd
Nimil Peethambaran <nimil.peethambaran@iq-ity.com>
2017-03-20 16:11:15 -0400

    fixed test cases

--- js/main/scratchPad.js
+++ js/main/scratchPad.js
@@ -350,7 +350,7 @@ function ScratchPadBuilder() {
         },
         convertToFabric = function(instance, drawer) {
             instance.canvas = new fabric.Canvas(instance.id, {isDrawingMode: true,stateful:true});
-			instance.canvas.setDimensions({width:instance.dimension.width, height:instance.dimension.height});
+//			instance.canvas.setDimensions({width:instance.dimension.width, height:instance.dimension.height});
             instance.canvas.freeDrawingBrush = new fabric.PencilBrush(instance.canvas);
             instance.canvas.freeDrawingBrush.width = 2;
             drawer.bindMouseDownEvents(instance, menuItems);
@@ -593,6 +593,7 @@ function ScratchPadDrawer() {
 
 			//instance.redo = [];
 			if(itemNums.length>0){
+				
 				instance.undo.push({
 				   "action" : _delete,
 				   "itemIndex": itemNums
